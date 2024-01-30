21f06308dd39ee43541245396d5921dcbe618800
monsterofaiml95 <135440369+monsterofaiml95@users.noreply.github.com>
2024-01-30 15:30:14 +0530

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/Universe")
+mongoose.connect("mongodb+srv://Admin:Admi1234@cluster0.ivmippx.mongodb.net/Universe")
 const postSchema = new mongoose.Schema({
   imageText: {
     type: String,
